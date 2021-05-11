c9ff827dba840e20f941353820c2dbf37100286e
SyedAakif <69403829+SyedAakif@users.noreply.github.com>
2021-05-12 03:44:23 +0500

    fixed

--- github-finder/src/components/context/alert/AlertReducer.js
+++ github-finder/src/components/context/alert/AlertReducer.js
@@ -1,6 +1,6 @@
 import { SET_ALERT, REMOVE_ALERT  } from '../types';
 
-const AlertReducer = (state, action) => {
+const alertReducer = (state, action) => {
     switch(action.type){
         case SET_ALERT:
             return action.payload;
@@ -12,4 +12,4 @@ const AlertReducer = (state, action) => {
     }
 }
 
-export default AlertReducer
+export default alertReducer
